209d0344d511e0b735113ab33735105d893a3648
Emmanuel Otuonye <emmaotuonye1@gmail.com>
2025-03-07 11:29:21 +0100

    new updates

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,32 @@
 import { Outlet } from "react-router-dom";
 import { FaPlay } from "react-icons/fa6";
+import { useSelector } from "react-redux";
+import { RootState } from "@/store";
 
 const Layout = () => {
+  const { form, files } = useSelector((state: RootState) => state);
+
+  const submitForm = () => {
+    fetch(`https://ai-dervet.onrender.com/use-case/ebr`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        ebr_parameter: form,
+        files: files.files,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => console.log("Success:", data))
+      .catch((error) => console.error("Error:", error));
+  };
+
   return (
     <div>
       <div className="w-full h-[56px] border flex items-center px-10">
-        <div className="p-3 border rounded-md ml-auto bg-blue-500 cursor-pointer">
+        <div
+          onClick={submitForm}
+          className="p-3 border rounded-md ml-auto bg-blue-500 cursor-pointer"
+        >
           <FaPlay color="white" />
         </div>
       </div>
--- src/store/index.ts
+++ src/store/index.ts
@@ -7,7 +7,7 @@ import {
 import { persistStore } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import { persistReducer } from "redux-persist";
-import { RootObject } from "./output.interface";
+import { IFile, RootObject } from "./output.interface";
 import inputJson from "./inputs.json";
 import set from "lodash/set";
 import cloneDeep from "lodash/cloneDeep";
@@ -63,9 +63,55 @@ const derSlice = createSlice({
   },
 });
 
+const fileInitialState: { files: Array<IFile> } = {
+  files: [
+    {
+      filename: "cycle_10487cf5-16d9-4b78-8109-440b29e1ef78.csv",
+      url: "https://storage.googleapis.com/carbon-adjust-file.appspot.com/dervet/5eaeea7d-1093-45ed-87f8-67a8ef55fcc9/cycle_10487cf5-16d9-4b78-8109-440b29e1ef78.csv",
+    },
+    {
+      filename: "yearly.csv",
+      url: "https://storage.googleapis.com/carbon-adjust-file.appspot.com/dervet/a483cf51-aada-4856-ad0b-3227230d0a8b/yearly.csv",
+    },
+    {
+      filename: "timeseries.csv",
+      url: "https://storage.googleapis.com/carbon-adjust-file.appspot.com/dervet/9b948379-88f8-4a49-b65d-5a464fbaaa1b/timeseries.csv",
+    },
+    {
+      filename: "tariff.csv",
+      url: "https://storage.googleapis.com/carbon-adjust-file.appspot.com/dervet/58aff115-e0d8-49b0-b2ff-3b742eecb684/tariff.csv",
+    },
+    {
+      filename: "monthly.csv",
+      url: "https://storage.googleapis.com/carbon-adjust-file.appspot.com/dervet/cb04d137-085a-404e-b33d-c0e8355849b8/monthly.csv",
+    },
+    {
+      filename: "load_shed_percentage.csv",
+      url: "https://storage.googleapis.com/carbon-adjust-file.appspot.com/dervet/6701d054-ce2d-46d1-9c28-f9465e11a9be/load_shed_percentage.csv",
+    },
+  ],
+};
+
+const fileSlice = createSlice({
+  name: "files",
+  initialState: fileInitialState,
+  reducers: {
+    updateFile: (state, action: PayloadAction<IFile>) => {
+      const fileIndex = state.files.findIndex(
+        (item) => item.filename === action.payload.filename
+      );
+
+      if (fileIndex !== -1) return;
+
+      state.files = [...state.files, action.payload];
+    },
+  },
+});
+
 const rootReducer = combineReducers({
   form: formSlice.reducer,
   der: derSlice.reducer,
+  files: fileSlice.reducer,
 });
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
@@ -77,6 +123,7 @@ const store = configureStore({
 // Slice actions
 export const { updateField } = formSlice.actions;
 export const { updateDer, removeDer } = derSlice.actions;
+export const { updateFile } = fileSlice.actions;
 
 export const persistor = persistStore(store);
 export type RootState = ReturnType<typeof store.getState>;
